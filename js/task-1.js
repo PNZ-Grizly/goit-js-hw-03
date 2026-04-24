980b8b95ab6a73cea0f2640071cb928b8a79cf27
Nazar Patsula <nazar.patcula88@gmail.com>
2026-04-24 02:01:37 +0200

    modified task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,5 @@
 function slugify(title) {
-  return title
-    .toLowerCase()
-    .trim()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]/g, '');
+  return title.toLowerCase().split(' ').join('-');
 }
 
 console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
